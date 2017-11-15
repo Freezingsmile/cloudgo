def28fb73af2f776d4325a372ec83fa7808e85b7
Freezingsmile <409871419@qq.com>
2017-11-16 00:36:52 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "os"
+    "Cloudgo/service"
+    flag "github.com/spf13/pflag"
+)
+
+const (
+    PORT string = "8080"
+)
+
+func main() {
+    port := os.Getenv("PORT")
+    if len(port) == 0 {
+        port = PORT
+    }
+    pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
+    flag.Parse()
+    if len(*pPort) != 0 {
+        port = *pPort
+        // points to the pPort
+	}
+    service.NewServer(port)
+}
